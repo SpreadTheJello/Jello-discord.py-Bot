9aef19bbeaa3aa0aa8070a3684143865b434aae3
SpreadTheJello <Anjello10fat@gmail.com>
2020-12-14 17:26:10 -0800

    version 0.0.1 ONLINE

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -5,39 +5,39 @@ PREFIX = "?"
 OWNER_IDS = [216768190984749057]
 
 class Bot(BotBase):
-	def __init__(self):
-		self.PREFIX = PREFIX
-		self.ready = False
-		self.guild = None
-		self.scheduler = AsyncIOScheduler()
+    def __init__(self):
+        self.PREFIX = PREFIX
+        self.ready = False
+        self.guild = None
+        self.scheduler = AsyncIOScheduler()
 
-		super().__init__(command_prefix=PREFIX, owner_ids=OWNER_IDS)
+        super().__init__(command_prefix=PREFIX, owner_ids=OWNER_IDS)
 
-def run(self, version):
-	self.VERSION = version
+    def run(self, version):
+        self.VERSION = version
 
-	with open("./lib/bot/token", "r", encoding="utf-8") as tf:
-		self.TOKEN = tf.read()
+        with open("./lib/bot/token", "r", encoding="utf-8") as tf:
+            self.TOKEN = tf.read()
 
-	print("running bot...")
-	super().run(self.TOKEN, reconnect=True)
+        print("running bot...")
+        super().run(self.TOKEN, reconnect=True)
 
-async def on_connect(self):
-	print("bot connected")
-		
-async def on_disconnect(self):
-	print("bot disconnected")
+    async def on_connect(self):
+        print("bot connected")
+            
+    async def on_disconnect(self):
+        print("bot disconnected")
 
-async def on_ready(self):
-	if not self.ready:
-		self.ready = True
-		self.guild = self.get_guild(788174344861646908)
-		print("bot ready")
+    async def on_ready(self):
+        if not self.ready:
+            self.ready = True
+            self.guild = self.get_guild(788174344861646908)
+            print("bot ready")
 
-	else:
-		print("bot reconnected")
+        else:
+            print("bot reconnected")
 
-async def on_message(self, message):
-	pass
+    async def on_message(self, message):
+        pass
 
 bot = Bot()
