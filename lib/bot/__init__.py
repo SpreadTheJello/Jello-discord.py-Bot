b84dc2a0abd1e95938ba84ee097b09c2a09d8f5f
SpreadTheJello <Anjello10fat@gmail.com>
2020-12-14 19:48:47 -0800

    Verison 0.0.3 Msg when Online + random quotes

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -1,7 +1,9 @@
 from discord import Intents
 from discord import Embed
+from random import choice
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from discord.ext.commands import Bot as BotBase
+
 
 PREFIX = "?"
 OWNER_IDS = [216768190984749057]
@@ -39,8 +41,23 @@ class Bot(BotBase):
 			self.ready = True
 			self.guild = self.get_guild(788174344861646908)
 			print("bot ready")
+
 			channel = self.get_channel(788234381327597589)
-			await channel.send("Bad boys, bad boys, watchu gonna do, watcha gonna do when they come for you")
+			#await channel.send("Bad boys, bad boys, watchu gonna do, watcha gonna do when they come for you")
+
+			quotes = ["Jump up and down like you're a pancake.",
+			"A steak a day keeps the vegan away!",
+			"I see you have a hat, tis a nice hat."]
+			quote = (choice(quotes))
+
+			embed = Embed(title="Jello Bot is online!", description="?help to get a list of commands.", color=0xFF0000)
+			fields = [("\a", quote, False)]
+
+			for name, value, inline in fields:
+				embed.add_field(name=name, value=value, inline=inline)
+				embed.set_footer(text="Quotes by Isaac")
+			await channel.send(embed=embed)
+			
 
 		else:
 			print("bot reconnected")
