34a2b5662e3af1adc89d651004512ec894c9c21f
SpreadTheJello <Anjello10fat@gmail.com>
2020-12-15 15:19:22 -0800

    0.0.8 Dice Command

--- lib/cogs/fun.py
+++ lib/cogs/fun.py
@@ -11,6 +11,13 @@ class Fun(Cog):
     async def say_hello(self, ctx):
         await ctx.send(f"{choice(('Hello', 'Hi', 'Yo', 'Hey', 'Sup'))} {ctx.author.mention}!")
 
+    @command(name="dice", asliases=["rull"])
+    async def roll_dice(self, ctx, die_string: str):
+        dice, value = (int(term) for term in die_string.split("d"))
+        rolls = [randint(1, value) for i in range(dice)]
+
+        await ctx.send(" + ".join([str(r) for r in rolls]) + f" = {sum(rolls)}")
+
     @Cog.listener()
     async def on_ready(self):
         if not self.bot.ready:
